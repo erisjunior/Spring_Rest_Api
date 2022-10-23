1d4320368c11225fdab1e2739617d4940b25c4ac
Antonio Erisvan <erisvan.junior.a@gmail.com>
2022-10-23 13:37:31 -0300

    feat: setup project and lint code

---
+++ src/main/java/com/jeanlima/springrestapi/rest/controllers/ApplicationControllerAdvice.java
@@ -0,0 +1,24 @@
+package com.jeanlima.springrestapi.rest.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.jeanlima.springrestapi.exception.RegraNegocioException;
+import com.jeanlima.springrestapi.rest.ApiErrors;
+
+@RestControllerAdvice // trata os exceptionHandlers --> trata erros quando elesa acontecem
+public class ApplicationControllerAdvice {
+
+    /*
+     * toda vez que api lançar essa exceção, cairá aqui!
+     * preciso dizer qual status será retornado - por padrão -bad request - 400
+     */
+    @ExceptionHandler(RegraNegocioException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleRegraNegocioException(RegraNegocioException ex) {
+        String mensagemErro = ex.getMessage();
+        return new ApiErrors(mensagemErro);
+    }
+}
---
+++ src/main/java/com/jeanlima/springrestapi/rest/dto/PedidoDTO.java
@@ -0,0 +1,31 @@
+package com.jeanlima.springrestapi.rest.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/*
+ * {
+    "cliente" : 1,
+    "total" : 100,
+    "items" : [
+        {
+            "produto": 1,
+            "quantidade": 10
+        }
+        
+    ]
+}
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PedidoDTO {
+    private Integer cliente;
+    private BigDecimal total;
+    private List<ItemPedidoDTO> items;
+}
